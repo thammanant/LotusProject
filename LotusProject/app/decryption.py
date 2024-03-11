9bcd513b6e1302e077bc16298b65dfe426f1b39f
Pun <80048553+thammanant@users.noreply.github.com>
2024-03-12 02:18:45 +0700

    seventh version

--- LotusProject/app/decryption.py
+++ LotusProject/app/decryption.py
@@ -1,22 +1,21 @@
 import os
-
-from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 
 
 class Decryption:
     def __init__(self):
         load_dotenv()
-        self.key = os.getenv('KEY')
+        key = os.getenv('KEY')
+        self.n, self.d = [int(x, 16) for x in key.split(',')]
 
-    def decrypt(self, message):
-        # TODO remove comments when done testing
-        # fernet = Fernet(self.key)
-        # message = fernet.decrypt(message.encode()).decode()
-        return message
+    def decrypt(self, encrypted_message):
+        # TODO remove comments and return when done testing
+        # encrypted_message = [int(encrypted_message)]
+        # decrypted_message = ''.join([chr(pow(int(char), self.d, self.n)) for char in encrypted_message])
+        # return decrypted_message
+        return encrypted_message
 
 
 # if __name__ == "__main__":
 #     decryption = Decryption()
-#     print(decryption.decrypt(
-#         "gAAAAABl6YBAOgRG5MQdk6-nKWphlu69_Dj5sIx6J0Wf4LxnN0WPg-XEJ2_WkH0mpkKCiPuCD_5PxrOSWfqdZkJw7bkw_NXZvA=="))
+#     print(decryption.decrypt("93170485217644808136623805722300481109594233128562405997672195557014364348133205538729907885288218963258194390116096938072677469277356766691940572264211050666569665841104200554353873142299571272735198503912924779488845867606627746162096269587458429549324453991981491067555768577864224883995387039985071551313"))
