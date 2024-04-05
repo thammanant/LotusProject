bd6001d94cac9d77cfa89e66b0732b3abb3c5760
Pun <80048553+thammanant@users.noreply.github.com>
2024-04-06 02:03:14 +0700

    fixed decrypt

--- LotusProject/app/decryption.py
+++ LotusProject/app/decryption.py
@@ -9,13 +9,18 @@ class Decryption:
         self.n, self.d = [int(x, 16) for x in key.split(',')]
 
     def decrypt(self, encrypted_message):
-        # TODO remove comments and return when done testing
-        encrypted_message = [encrypted_message]
-        decrypted_message = [chr(pow(char, self.d, self.n)) for char in encrypted_message]
+        # separate by %2C
+        encrypted_message = encrypted_message.split('%2C')
+        print(encrypted_message)
+        decrypted_message = []
+        for i in encrypted_message:
+            i = int(i)
+            decrypted_message.append(chr(pow(i, self.d, self.n)))
+
         return ''.join(decrypted_message)
         # return encrypted_message
 
 
 # if __name__ == "__main__":
 #     decryption = Decryption()
-#     print(decryption.decrypt(3931946207050845389613283327554538059130452635054030560366918972713655765235634179403788805931045307253404522531524182458929899383354584013253351837339141113181187478507027021631004459756582607400756809078244794981441783884766057823141242374969734608334111960568365416050055475626082131581947173752574833575))
+#     print(decryption.decrypt("82797263122673622252398609254816336932239895641311134141567714134294435441920468572069431727612875451590765722563750610444743739647654043451189443008711099411881516504023383958426332148078113117728534264956495158706799921811190059563764862096717623117364576125108214657489794083908300707317521383930137922526"))
